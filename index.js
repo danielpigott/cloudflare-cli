ff9ff8694bb462644d15d114de1441337325cc00
Daniel <pigottd@gmail.com>
2016-10-03 22:41:31 +1100

    Implementing functions for v2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function CloudflareCli(options) {
     var self = this;
+    var _ = require('lodash');
     var CFClient = require('cloudflare');
     var fs = require('fs');
     var format = require('util').format;
@@ -15,6 +16,21 @@ function CloudflareCli(options) {
             shortcut: 'h',
             callback: showHelp,
             description: 'Show help'
+        },
+        purge: {
+            aliases: ['purgefile'],
+            callback: purgeCache,
+            description: 'Purge files from cache'
+        },
+        devmode: {
+            aliases: [],
+            callback: toggleDevMode,
+            description: "Turn dev mode on or off"
+        },
+        rm : {
+            aliases: ['removerecord'],
+            callback: removeRecord,
+            description: 'Remove a record'
         }
     };
     var client;
@@ -32,6 +48,7 @@ function CloudflareCli(options) {
     }
 
     function runCommand(command, options) {
+
         if (commands[command] != undefined) {
             var fn = commands[command].callback;
             fn(options);
@@ -69,8 +86,27 @@ function CloudflareCli(options) {
 
     }
 
+    /**
+     * Remove a record/record(s) matching the given name
+     * @param options
+     */
     function removeRecord(options) {
-
+        getZone(options.domain).then(function(zone) {
+            return client.browseDNS(zone, {name: options._[1]});
+        }).then(function(records) {
+            if (records.count == 0) {
+                throw new Error('No matching records found');
+            }
+            var results = [];
+            _.each(records.result, function (record) {
+                results.push(client.deleteDNS(record));
+            });
+            return Promise.all(results);
+        }).then(function(result) {
+            console.log(result);
+        }).catch(function(error){
+            console.log(error);
+        });
     }
 
     function listRecords(options) {
@@ -78,17 +114,32 @@ function CloudflareCli(options) {
     }
 
     function toggleDevMode(options) {
+        getZone(options.domain).then(function (zone) {
+            zone.devMode = (options._[1] == 'on');
+            return client.editZone(zone);
 
+        }).then(function(result) {
+            console.log(result);
+        }).catch(function(error) {
+            console.log(error);
+        });
     }
 
+    /**
+     * Purge files from cache
+     * @param options
+     */
     function purgeCache(options) {
-
+        getZone(options.domain).then(function (zone) {
+            var query = (options._[1]) ? {files : [options._[1]]} :  {purge_everything : true};
+            return client.deleteCache(zone, query);
+        }).then(function(result) {
+            console.log(result);
+        }).catch(function(err) {
+            console.log(err);
+        });
     }
-
-    function purgeFile(options) {
-
-    }
-
+    
     /**
      *
      * @param zoneName {String}
