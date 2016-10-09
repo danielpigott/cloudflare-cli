8387e57cd4d78199375a0029ef8863852ec04839
Daniel <pigottd@gmail.com>
2016-10-09 22:50:52 +1100

    Refactoring

--- index.js
+++ index.js
@@ -51,35 +51,37 @@ function CloudflareCli(options) {
 
         if (commands[command] != undefined) {
             var fn = commands[command].callback;
-            fn(options);
+            fn(options).then(function(result) {
+              console.log(result);
+            }).catch(function(error) {
+                console.log(error);
+            });
         } else {
             console.log(allowedOptions);
         }
     }
 
     function addRecord(options) {
-        getZone(options.domain).then(function (zone) {
-            var record = CFClient.DNSRecord.create({
+        return getZone(options.domain).then(
+            function (zone) {
+            return CFClient.DNSRecord.create({
                 zoneId: zone.id,
                 type: options.type,
                 name: options._[1],
                 content: options._[2],
                 ttl: options.ttl || 1,
                 prio: options.priority || 0
-            });
-            client.addDNS(record).then(function (newRecord) {
+            })})
+            .then(function(record) {
+            return client.addDNS(record).then(function (newRecord) {
                 console.log(format(
                     'Added %s record %s -> %s',
                     newRecord.type,
                     newRecord.name,
                     newRecord.content)
                 );
-            }).catch(function(err) {
-                console.log(err);
             });
-        }).catch(function (err) {
-            console.log(err);
-        });
+        })
     }
 
     function editRecord(options) {
@@ -91,7 +93,7 @@ function CloudflareCli(options) {
      * @param options
      */
     function removeRecord(options) {
-        getZone(options.domain).then(function(zone) {
+        return getZone(options.domain).then(function(zone) {
             return client.browseDNS(zone, {name: options._[1]});
         }).then(function(records) {
             if (records.count == 0) {
@@ -102,10 +104,6 @@ function CloudflareCli(options) {
                 results.push(client.deleteDNS(record));
             });
             return Promise.all(results);
-        }).then(function(result) {
-            console.log(result);
-        }).catch(function(error){
-            console.log(error);
         });
     }
 
@@ -128,15 +126,12 @@ function CloudflareCli(options) {
     /**
      * Purge files from cache
      * @param options
+     * @returns {Promise}
      */
     function purgeCache(options) {
-        getZone(options.domain).then(function (zone) {
+        return getZone(options.domain).then(function (zone) {
             var query = (options._[1]) ? {files : [options._[1]]} :  {purge_everything : true};
             return client.deleteCache(zone, query);
-        }).then(function(result) {
-            console.log(result);
-        }).catch(function(err) {
-            console.log(err);
         });
     }
     
