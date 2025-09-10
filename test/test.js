f7750092f90de5b4188fda4c90531520b03b6d1f
Daniel Pigott <danielp@canva.com>
2025-09-11 00:06:06 +1000

    Upgraded dependencies and code

--- test/test.js
+++ test/test.js
@@ -1,14 +1,15 @@
-const CloudflareCli = require('../index');
-const ConfigReader = require('../lib/configReader');
-const uuid = require('uuid/v4');
-const _ = require('lodash');
+import { CloudflareCli } from './../index.js';
+import { ConfigReader } from './../lib/configReader.js';
+import { v4 as uuid } from 'uuid';
+import _ from 'lodash';
 
-const assert = require('assert');
+import { equal } from 'assert';
 
 const config = new ConfigReader().readConfig();
 const cli = new CloudflareCli(config);
-const zoneName = 'cloudflaretest.com';
+const zoneName = 'afklafljlaf.com';
 const recordName = uuid();
+const recordContent = `${Math.floor(Math.random() * 10)}.${Math.floor(Math.random() * 10)}.${Math.floor(Math.random() * 10)}.${Math.floor(Math.random() * 10)}`;
 const requiredEnvVars = [
   'CF_API_KEY',
 ];
@@ -22,54 +23,49 @@ _.each(requiredEnvVars, function (envVar) {
 
 describe('CloudflareCli', function () {
   this.timeout(20000);
-  it('should use environment variables where available', function () {
-    assert.equal(cli.key, process.env.CF_API_KEY);
-    assert.equal(cli.email, process.env.CF_API_EMAIL);
+  it('should use environment variables where available', async function () {
+    equal(cli.key, process.env.CF_API_KEY);
+    equal(cli.email, process.env.CF_API_EMAIL);
   });
-  it('should add a zone', function(done) {
-    cli.addZone({name: zoneName}).then(
-      function () {
-        done();
-      }
-    ).catch(function(error) {
+  it('should add a zone', async function () {
+    try {
+      await cli.addZone({ name: zoneName });
+    } catch (error) {
       console.log(error);
-      done(error);
-    });
+      if (error.response && error.response.data && error.response.data.errors) {
+        console.log(error.response.data.errors);
+      }
+      throw error;
+    }
   });
-  it('should add an A record', function (done) {
-    cli.addRecord({
+  it('should add an A record', async function () {
+    await cli.addRecord({
       'domain': zoneName,
       'type': 'A',
       'name': recordName,
-      'content': '8.8.8.8'
-    }).then(function () {
-      done();
-    }).catch(function (error) {
-      done(error);
+      'content': recordContent,
     });
   });
-  it('should find an A record', function (done) {
-    cli.findRecord({domain: zoneName, 'name': recordName}).then(function (result) {
-      assert.equal(result.messages[0].name, `${recordName}.${zoneName}`);
-      done();
-    });
+  it('should find an A record', async function () {
+    const result = await cli.findRecord({ domain: zoneName, 'name': recordName });
+    equal(result.messages[0].name, `${recordName}.${zoneName}`);
   });
-  it('should find an A record using a query', async () => {
-    const result = await cli.findRecord({domain: zoneName, query: {content: '8.8.8.8'}});
-    assert.equal(result.messages[0].name, `${recordName}.${zoneName}`);
+  it('should find an A record using a query', async function () {
+    const result = await cli.findRecord({ domain: zoneName, query: { content: recordContent } });
+    equal(result.messages[0].name, `${recordName}.${zoneName}`);
   });
-  it('should not find an A record when using a non-matching query', async () => {
-    const result = await cli.findRecord({domain: zoneName, query: {content: 'xyz'}});
-    assert.equal(result.messages.length,0);
+  it('should find an A record querying by name', async function () {
+    const result = await cli.findRecord({ domain: zoneName, query: { name: recordName } });
+    equal(result.messages[0].name, `${recordName}.${zoneName}`);
   });
-  it('should remove an A record', function (done) {
-    cli.removeRecord({'domain': zoneName, 'name': recordName}).then(function () {
-      done();
-    });
+  it('should not find an A record when using a non-matching query', async function () {
+    const result = await cli.findRecord({ domain: zoneName, query: { content: 'xyz' } });
+    equal(result.messages.length, 0);
   });
-  it('should remove a zone', function (done) {
-    cli.removeZone({name: zoneName}).then(function() {
-      done();
-    });
-  })
+  it('should remove an A record', async function () {
+    await cli.removeRecord({ 'domain': zoneName, 'name': recordName });
+  });
+  it('should remove a zone', async function () {
+    await cli.removeZone({ name: zoneName });
+  });
 });
